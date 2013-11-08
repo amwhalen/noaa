6c734e3244988768755ca10b1d4b10abc6e0fdf7
amwhalen <awhalen@wpi.edu>
2013-11-08 09:41:29 -0500

    added link to weather conditions documentation

--- noaa/weather/response/Conditions.php
+++ noaa/weather/response/Conditions.php
@@ -20,6 +20,7 @@ class Conditions extends Base {
 	/**
 	 * String representation
 	 * Returns a string like "Chance Rain Showers (chance light rain showers and patchy fog)"
+	 * See the XML documentation for all possible values of these fields: http://www.nws.noaa.gov/xml/xml_fields_icon_weather_conditions.php
 	 */
 	public function __toString() {
 		
@@ -33,12 +34,12 @@ class Conditions extends Base {
 				
 				$words = array();
 
-				// does this day have multiple conditions? the additive will be set to something like "and"
+				// does this day have multiple conditions? the additive will be set to "and" or "or"
 				if (isset($v['additive'])) {
 					$words[] = $v['additive'];
 				}
 
-				// "chance", "patchy", etc.
+				// "chance", "patchy", "intermittent", etc.
 				$words[] = $v['coverage'];
 
 				// ignore "none" for intensity. We don't want to say "patchy none fog", but rather "patchy fog" instead.
@@ -47,7 +48,7 @@ class Conditions extends Base {
 					$words[] = $v['intensity'];
 				}
 
-				// "rain showers", "fog", "snow", etc.
+				// "rain showers", "thunderstorms", "fog", "snow", etc.
 				$words[] = $v['weather-type'];
 
 				// convert all the attributes into a string like "slight chance light rain showers"
